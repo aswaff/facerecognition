00d747ef2cd27d652d83b2507c830824524b294c
aswaff <44035215+aswaff@users.noreply.github.com>
2020-04-15 09:58:15 -0400

    Changed fetch from local host to direct to heroku

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://safe-sierra-78486.herokuapp.com/imageurl', {
           method: 'post',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -84,7 +84,7 @@ class App extends Component {
         .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://safe-sierra-78486.herokuapp.com:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
